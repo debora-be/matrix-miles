2e583c74f9d3270a6dc0b1cae31a63f5554b87f0
debora berte <arobedbee@gmail.com>
2023-10-13 13:36:59 -0300

    First commit

---
+++ lib/matrix_miles.ex
@@ -0,0 +1,18 @@
+defmodule MatrixMiles do
+  @moduledoc """
+  Documentation for `MatrixMiles`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MatrixMiles.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/matrix_miles_test.exs
@@ -0,0 +1,8 @@
+defmodule MatrixMilesTest do
+  use ExUnit.Case
+  doctest MatrixMiles
+
+  test "greets the world" do
+    assert MatrixMiles.hello() == :world
+  end
+end
