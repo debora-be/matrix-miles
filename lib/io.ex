4ffbac3b1da1bffa9426f2766cd90dc74a88d170
debora berte <arobedbee@gmail.com>
2023-10-15 15:53:27 -0300

    Create input/output interface

---
+++ lib/io.ex
@@ -0,0 +1,63 @@
+defmodule Io do
+  @moduledoc """
+  Input/Output module for MatrixMiles application.
+  """
+
+  @doc """
+  Starts the input/output interaction.
+  """
+  @spec start() :: :ok
+  def start do
+    IO.puts("Welcome to the MatrixMiles Martian Vehicle Navigation System!")
+
+    platform_size = get_platform_size()
+    number_of_vehicles = get_number_of_vehicles()
+
+    vehicle_data = Enum.map(1..number_of_vehicles, fn _ ->
+      get_vehicle_data()
+    end)
+
+    MatrixMiles.call(platform_size, vehicle_data)
+  end
+
+  defp get_platform_size do
+    IO.puts("Enter the matrix size (for example, '5 5'):")
+
+    [x, y] =
+      "> "
+      |> IO.gets()
+      |> String.split()
+      |> Enum.map(&format/1)
+
+    {x, y}
+  end
+
+  defp get_number_of_vehicles do
+    IO.puts("Enter the number of vehicles:")
+
+    "> "
+    |> IO.gets()
+    |> String.trim()
+    |> format()
+  end
+
+  defp get_vehicle_data do
+    IO.puts("Enter vehicle's initial position (for example, '1 2 N'):")
+
+    [x, y, direction] =
+      "> "
+      |> IO.gets()
+      |> String.split()
+
+    IO.puts("Enter vehicle's movements (for example, 'EMEMEMEMM'):")
+
+    movements =
+      "> "
+        |> IO.gets()
+        |> String.trim()
+
+    {%Vehicle{x: format(x), y: format(y), direction: direction}, movements}
+  end
+
+  defp format(data), do: String.to_integer(data)
+end
